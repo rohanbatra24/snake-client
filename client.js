bf8c0088e8017ae36c88b6fb128eae39e1919bc9
Rohan Batra <rohanbatra24@gmail.com>
2020-03-11 20:54:46 +0000

    added direction movement

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ const net = require('net');
 
 const connect = function() {
   const conn = net.createConnection({
-    host: '10.0.2.15',
+    host: '172.46.0.203',
     port: 50541
   });
 
--- play.js
+++ play.js
@@ -1,26 +1,5 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  stdin.on('data', (key) => {
-    handleUserInput(key);
-  });
-
-  return stdin;
-};
-
-console.log('Connecting...');
-
-setupInput();
-
-connect();
+console.log('Connecting ...');
+setupInput(connect());
