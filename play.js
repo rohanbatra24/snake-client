89e8558b99500408755a897d8bfc9293fe9772b3
Rohan Batra <rohanbatra24@gmail.com>
2020-03-11 18:09:10 +0000

    created client.js

--- play.js
+++ play.js
@@ -1,19 +1,5 @@
-const net = require('net');
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: '10.0.2.15',
-    port: 50541
-  });
-
-  conn.setEncoding('utf8');
-
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-
-  return conn;
-};
+const { connect } = require('./client');
 
 console.log('Connecting...');
+
 connect();
